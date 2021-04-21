3a0004598faf0a77e67b37cf836a61dd4940fb11
Arief Rifqy M. J. N <82900246+armjn7@users.noreply.github.com>
2021-04-21 11:14:31 +0700

    Add files via upload

---
+++ halo.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("Program halo.py")
+print("by ariefrifqy@mail.ugm.ac.id")
+
+
+# In[3]:
+
+
+
+print("#|================================|")
+print("#|                                |")
+print("#|        Program halo.py         |")
+print("#|  by ariefrifqy@mail.ugm.ac.id  |")
+print("#|                                |")
+print("#|================================|")
+
+
+# In[ ]:
+
+
+
+
